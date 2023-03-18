9dcbef9c951db1f4c12469806b1cea3251f0cbf5
amityadav11 <amityadav11@hotmail.com>
2023-03-18 08:02:56 +0530

    updated-Redux-product

--- src/Redux/productReducer/action.js
+++ src/Redux/productReducer/action.js
@@ -1,14 +1,34 @@
 import axios from "axios";
-import { ADD_PRODUCT_FAILURE, ADD_PRODUCT_REQUEST, ADD_PRODUCT_SUCCESS } from "./actionTypes";
+import {
+  PRODUCT_FAILURE,
+  PRODUCT_REQUEST,
+  ADD_PRODUCT_SUCCESS,
+  GET_PRODUCT_SUCCESS,
+} from "./actionTypes";
 
-export const addProduct = (data) => (dispatch) =>{
-   dispatch({type:ADD_PRODUCT_REQUEST});
+export const addProduct = (data) => (dispatch) => {
+  dispatch({ type: PRODUCT_REQUEST });
 
-   axios.post("http://localhost:8080/products",data)
-        .then(()=>{
-            dispatch({type:ADD_PRODUCT_SUCCESS});
-        })
-        .catch(()=>{
-            dispatch({type:ADD_PRODUCT_FAILURE})
-        })
-}
+  axios
+    .post("http://localhost:8080/products", data)
+    .then(() => {
+      dispatch({ type: ADD_PRODUCT_SUCCESS });
+    })
+    .catch(() => {
+      dispatch({ type: PRODUCT_FAILURE });
+    });
+};
+
+
+export const getProducts =  (dispatch) => {
+    dispatch({ type: PRODUCT_REQUEST });
+  
+    axios
+      .get("http://localhost:8080/products")
+      .then(() => {
+        dispatch({ type: GET_PRODUCT_SUCCESS });
+      })
+      .catch(() => {
+        dispatch({ type: PRODUCT_FAILURE });
+      });
+  };
